7101c07f6bf7fd266f6b8c9124a5bf41dae57cde
tgrafton <graftontt@gmail.com>
2015-08-27 23:32:17 +0100

    Added next and prev to ConnectionDirection to make it easier to implement nextOrientation and prevOrientation in Piece implementations.

--- infinitysolver/src/main/java/meindratheal/solvers/infinitysolver/piece/ConnectionDirection.java
+++ infinitysolver/src/main/java/meindratheal/solvers/infinitysolver/piece/ConnectionDirection.java
@@ -10,4 +10,38 @@ public enum ConnectionDirection
 	EAST,
 	SOUTH,
 	WEST;
+
+	/**
+	 * Gets the next ConnectionDirection according to ordinal.
+	 * @return The next direction.
+	 */
+	public ConnectionDirection next()
+	{
+		final ConnectionDirection[] vals = values();
+		if(this.ordinal() == vals.length - 1)
+		{
+			return vals[0];
+		}
+		else
+		{
+			return vals[this.ordinal() + 1];
+		}
+	}
+
+	/**
+	 * Gets the previous ConnectionDirection according to ordinal.
+	 * @return The previous direction.
+	 */
+	public ConnectionDirection prev()
+	{
+		final ConnectionDirection[] vals = values();
+		if(this.ordinal() == 0)
+		{
+			return vals[vals.length - 1];
+		}
+		else
+		{
+			return vals[this.ordinal() - 1];
+		}
+	}
 }
